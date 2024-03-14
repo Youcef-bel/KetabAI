b8ecffe4d74030fa47b427683cff21a9807e37d0
Youcef Belmokhtar <78687916+Youcef-bel@users.noreply.github.com>
2024-03-14 17:20:18 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,68 +1,85 @@
 import streamlit as st
 from dotenv import load_dotenv
+import pickle
+from PyPDF2 import PdfReader
 from streamlit_extras.add_vertical_space import add_vertical_space
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.indexes import VectorstoreIndexCreator
-from langchain.document_loaders import UnstructuredPDFLoader
-from langchain.chains import RetrievalQA
-
-from langchain.embeddings import GooglePalmEmbeddings
-from langchain.llms import GooglePalm
-import os, glob
-
-
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.llms import OpenAI
+from langchain.chains.question_answering import load_qa_chain
+from langchain.callbacks import get_openai_callback
+import os
+ 
 # Sidebar contents
 with st.sidebar:
-    st.title("🤗💬 LLM Chat App")
-    st.markdown(
-        """
+    st.title('🤗💬 LLM Chat App')
+    st.markdown('''
     ## About
     This app is an LLM-powered chatbot built using:
     - [Streamlit](https://streamlit.io/)
     - [LangChain](https://python.langchain.com/)
-    - [PaLM](https://makersuite.google.com/app/home) Embeddings & LLM model
+    - [OpenAI](https://platform.openai.com/docs/models) LLM model
  
-    """
-    )
+    ''')
     add_vertical_space(5)
-    st.write("Made with ❤️ by [Prompt Engineer](https://youtube.com/@engineerprompt)")
-
+    st.write('Made with ❤️ by [Prompt Engineer](https://youtube.com/@engineerprompt)')
+ 
 load_dotenv()
-
-
+ 
 def main():
     st.header("Chat with PDF 💬")
-
-    files_path = "./SOURCE_DOCUMENTS/Orca_paper.pdf"
-    loaders = [UnstructuredPDFLoader(files_path)]
-
-    # if "index" not in st.session:
-    index = VectorstoreIndexCreator(
-        embedding=GooglePalmEmbeddings(),
-        text_splitter=RecursiveCharacterTextSplitter(chunk_size=800, chunk_overlap=0),
-    ).from_loaders(loaders)
-
-    llm = GooglePalm(temperature=0.1)  # OpenAI()
-    chain = RetrievalQA.from_chain_type(
-        llm=llm,
-        chain_type="stuff",
-        retriever=index.vectorstore.as_retriever(),
-        # input_key="question",
-        return_source_documents=True,
-    )
-
-    # st.session.index = index
-
-    # Accept user questions/query
-    query = st.text_input("Ask questions about your PDF file:")
-    # st.write(query)
-    if query:
-        response = chain(query)
-        st.write(response["result"])
-        with st.expander("Returned Chunks"):
-            for doc in response["source_documents"]:
-                st.write(f"{doc.metadata['source']} \n {doc.page_content}")
-
-
-if __name__ == "__main__":
+ 
+ 
+    # upload a PDF file
+    pdf = st.file_uploader("Upload your PDF", type='pdf')
+ 
+    # st.write(pdf)
+    if pdf is not None:
+        pdf_reader = PdfReader(pdf)
+        
+        text = ""
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+ 
+        text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size=1000,
+            chunk_overlap=200,
+            length_function=len
+            )
+        chunks = text_splitter.split_text(text=text)
+ 
+        # # embeddings
+        store_name = pdf.name[:-4]
+        st.write(f'{store_name}')
+        # st.write(chunks)
+ 
+        if os.path.exists(f"{store_name}.pkl"):
+            with open(f"{store_name}.pkl", "rb") as f:
+                VectorStore = pickle.load(f)
+            # st.write('Embeddings Loaded from the Disk')s
+        else:
+            embeddings = OpenAIEmbeddings()
+            VectorStore = FAISS.from_texts(chunks, embedding=embeddings)
+            with open(f"{store_name}.pkl", "wb") as f:
+                pickle.dump(VectorStore, f)
+ 
+        # embeddings = OpenAIEmbeddings()
+        # VectorStore = FAISS.from_texts(chunks, embedding=embeddings)
+ 
+        # Accept user questions/query
+        query = st.text_input("Ask questions about your PDF file:")
+        # st.write(query)
+ 
+        if query:
+            docs = VectorStore.similarity_search(query=query, k=3)
+ 
+            llm = OpenAI()
+            chain = load_qa_chain(llm=llm, chain_type="stuff")
+            with get_openai_callback() as cb:
+                response = chain.run(input_documents=docs, question=query)
+                print(cb)
+            st.write(response)
+ 
+if __name__ == '__main__':
     main()
