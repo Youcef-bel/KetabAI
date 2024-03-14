51e9bf8bbdb74904df21e6e3f1d79befbac889f6
Youcef Belmokhtar <78687916+Youcef-bel@users.noreply.github.com>
2024-03-14 18:57:36 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from dotenv import load_dotenv
 import pickle
-import google.generativeai
+import google.generativeai as GoogleGenerativeAI
 from PyPDF2 import PdfReader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.embeddings import GooglePalmEmbeddings
